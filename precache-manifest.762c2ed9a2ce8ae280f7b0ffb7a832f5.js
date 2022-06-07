b80897dff7eaeffafad94056e9eb62a6bf0d80c5
kir-dev13 <kirill87rabota@gmail.com>
2022-06-07 17:04:04 +0300

    Updates

---
+++ precache-manifest.762c2ed9a2ce8ae280f7b0ffb7a832f5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4ad4a5556aaa6aac4798a725cc97399",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad560a9d73f218dce16d",
+    "url": "/static/css/2.f7375132.chunk.css"
+  },
+  {
+    "revision": "6d25fceaf1e1d8a0c932",
+    "url": "/static/css/main.182354be.chunk.css"
+  },
+  {
+    "revision": "ad560a9d73f218dce16d",
+    "url": "/static/js/2.d218e379.chunk.js"
+  },
+  {
+    "revision": "7938e325320b6f16edcee8383dd99fa2",
+    "url": "/static/js/2.d218e379.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d25fceaf1e1d8a0c932",
+    "url": "/static/js/main.d679f548.chunk.js"
+  },
+  {
+    "revision": "e368430100bc2eed2d2b",
+    "url": "/static/js/runtime-main.9432879c.js"
+  },
+  {
+    "revision": "898d65d07b3ff005f2afa1f917b5f6da",
+    "url": "/static/media/background.898d65d0.jpg"
+  },
+  {
+    "revision": "37234ecccc5cbf084dad80b38960f24e",
+    "url": "/static/media/footer.37234ecc.jpg"
+  }
+]);
